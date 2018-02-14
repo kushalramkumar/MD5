8e74162e205c11557835f4382f8866d575b7d01f
kushalramkumar <kushal.ramkumar@gmail.com>
2018-02-14 07:47:11 +0530

    Added padding logic

--- md5.c
+++ md5.c
@@ -3,35 +3,72 @@
 #include <string.h>
 #include <stdint.h>
 
+#define SAFE_DELETE(x) do{\
+    if(NULL != x){ free(x); x = NULL; }\
+}while(0);
+
 const char gPlaintext[] = "Implement the message digest algorithm MD5 and test how random the output appears.\
 For example, test the percentage of 1 bits in the output, or test how many bits of output\
 change with minor changes in the input. Also, design various simplifications of the\
 message digest functions (such as reducing the number of rounds) and see how these\
 change things.";
 
+typedef struct
+{
+    uint8_t* data;
+    uint64_t length;
+}buffer_t;
+
+buffer_t* MD5Init(const uint8_t* xInputData)
+{
+    buffer_t* pBuffer = NULL;
+    if (xInputData)
+    {
+        uint32_t nPaddingBits = 0;
+        pBuffer = (buffer_t *)malloc(sizeof(buffer_t));
+        pBuffer->length = strlen(xInputData);
+        uint32_t mod = ((strlen(xInputData) * 8) % 512);
+        if (mod > 448)
+        {
+            nPaddingBits = 448 + (512 - mod);
+        }
+        else
+        {
+            nPaddingBits = 448 - mod;
+        }
+
+        if (0 < nPaddingBits)
+        {
+            printf("Creating the pad\n");
+            pBuffer->data = (uint8_t *)malloc(strlen(xInputData) + (nPaddingBits / 8) + 8);
+            if (NULL != pBuffer->data)
+            {
+                pBuffer->length = strlen(xInputData) + (nPaddingBits / 8) + 8;
+            }
+            memcpy(pBuffer->data, xInputData, strlen(xInputData));
+            pBuffer->data[strlen(xInputData)] = (char)'0x01';
+            memset(pBuffer->data + strlen(xInputData) + 1, '\0', (nPaddingBits / 8) - 1);
+            uint64_t nInputLength = strlen(xInputData);
+            memcpy(pBuffer->data + pBuffer->length - 8, &nInputLength, sizeof(nInputLength));
+        }
+    }
+    
+    return pBuffer;
+}
+
 int main(int argc, char** argv)
 {
-    printf("Length: %d \nLength mod 512 : %d\n", strlen(gPlaintext), ((strlen(gPlaintext) * 8) % 512));
-    uint8_t* pInputBlock = NULL;
-    uint32_t nPaddingBits = 448 - ((strlen(gPlaintext) * 8) % 512);
-
-    printf("nPaddingBits: %d pInputBlock length: %d\n", nPaddingBits, ((strlen(gPlaintext) + (nPaddingBits / 8))*8) % 512);
-    if (0 < nPaddingBits)
+    buffer_t* pBuffer = MD5Init(gPlaintext);
+    if (pBuffer)
     {
-        printf("Creating the pad\n");
-        pInputBlock = (uint8_t *)malloc(strlen(gPlaintext) + (nPaddingBits / 8) + 8);
-        memcpy(pInputBlock, gPlaintext, strlen(gPlaintext));
-        pInputBlock[strlen(gPlaintext)] = (char)'0x01';
-        memset(pInputBlock + strlen(gPlaintext) + 1, '\0', (nPaddingBits / 8) - 1);
-        uint64_t nInputBlock = strlen(gPlaintext);
-        memcpy(pInputBlock + strlen(gPlaintext) + (nPaddingBits / 8), &nInputBlock, sizeof(nInputBlock));
+        printf("Pre-processed input:\n");
+        for (uint32_t i = 0; i < pBuffer->length; i++)
+        {
+            printf("%d", pBuffer->data[i]);
+        }
+        printf("\n");
     }
-    printf("Pre-processed input:\n");
-    for (uint32_t i = 0; i < (strlen(gPlaintext) + (nPaddingBits / 8) + 8); i++)
-    {
-        printf("%d", pInputBlock[i]);
-    }
-    printf("\n");
-    free(pInputBlock);
+    
+    SAFE_DELETE(pBuffer);
     return 0;
 }
